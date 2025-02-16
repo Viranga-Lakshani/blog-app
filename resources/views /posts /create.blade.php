0a495ffbc09152dbef0b86a48da7004a38326a6b
Viranga Lakshani <104049331+Viranga-Lakshani@users.noreply.github.com>
2025-02-16 08:53:20 +0530

    Update create.blade.php9

--- resources/views /posts /create.blade.php
+++ resources/views /posts /create.blade.php
@@ -1,1 +1,31 @@
- 
+ @extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Create Post</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('posts.store') }}">
+                        @csrf
+
+                        <div class="form-group">
+                            <label for="title">Title</label>
+                            <input type="text" class="form-control" id="title" name="title" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="content">Content</label>
+                            <textarea class="form-control" id="content" name="content" rows="3" required></textarea>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
