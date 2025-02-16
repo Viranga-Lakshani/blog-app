02de8b5de11dc87c4cb8c40cc2c443c73422c10e
Viranga Lakshani <104049331+Viranga-Lakshani@users.noreply.github.com>
2025-02-16 06:56:06 +0530

    Create App.vue

---
+++ resources/js /components/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Laravel and Vue.js Mini App</h1>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style scoped>
+</style>
